f19f802d85a7250ff43cf80ce24dfbcf0f9e0a25
xuwenyihust <1016635786@qq.com>
2017-05-07 13:26:47 -0700

    Added test_access_lines_method().

--- tests/log_generator_test.py
+++ tests/log_generator_test.py
@@ -7,11 +7,11 @@ import re
 
 
 def test_heartbeat_lines_format():
-	gen = apache(out_path='./test_heartbeat_lines.txt', lines=['heartbeat'], forever=False, count=1)
+	gen = apache(out_path='./test_heartbeat_lines_format.txt', lines=['heartbeat'], forever=False, count=1)
 	gen.run()
 	
 	try:
-		f = open('./test_heartbeat_lines.txt')
+		f = open('./test_heartbeat_lines_format.txt')
 		line = f.readlines()[0]
 		#fields = line.split()
 		#assert len(fields) == 8
@@ -27,11 +27,11 @@ def test_heartbeat_lines_format():
 
 	
 def	test_access_lines_format():
-	gen = apache(out_path='./test_access_lines.txt', lines=['access'], forever=False, count=1)
+	gen = apache(out_path='./test_access_lines_format.txt', lines=['access'], forever=False, count=1)
 	gen.run()
 
 	try:
-		f = open('./test_access_lines.txt')
+		f = open('./test_access_lines_format.txt')
 		line = f.readlines()[0]
 		# Extract the time field
 		log_time = re.findall(r'\[(.*?)\]', line)
@@ -42,6 +42,78 @@ def	test_access_lines_format():
 
 	except:
 		assert False
+
+
+def test_access_lines_method():
+	# Test GET generation
+	gen = apache(out_path='./test_access_lines_method.txt', lines=['access'], methods=['GET'], forever=False, count=3)
+	gen.run()
+
+	try:
+		f = open('./test_access_lines_method.txt')
+		lines = f.readlines()
+		for line in lines:
+			# Extract the message field
+			log_msg = re.findall(r'\"(.*?)\"', line)[0]
+			# Extract the http method
+			log_method = log_msg.split()[0]
+			assert log_method == 'GET'
+
+	except:
+		assert False
+
+	# Test PUT generation
+	gen = apache(out_path='./test_access_lines_method.txt', lines=['access'], methods=['PUT'], forever=False, count=3)
+	gen.run()
+
+	try:
+		f = open('./test_access_lines_method.txt')
+		lines = f.readlines()
+		for line in lines:
+			# Extract the message field
+			log_msg = re.findall(r'\"(.*?)\"', line)[0]
+			# Extract the http method
+			log_method = log_msg.split()[0]
+			assert log_method == 'PUT'
+
+	except:
+		assert False
+
+	# Test POST generation
+	gen = apache(out_path='./test_access_lines_method.txt', lines=['access'], methods=['POST'], forever=False, count=3)
+	gen.run()
+
+	try:
+		f = open('./test_access_lines_method.txt')
+		lines = f.readlines()
+		for line in lines:
+			# Extract the message field
+			log_msg = re.findall(r'\"(.*?)\"', line)[0]
+			# Extract the http method
+			log_method = log_msg.split()[0]
+			assert log_method == 'POST'
+	except:
+		assert False
+
+	# Test DELETE generation
+	gen = apache(out_path='./test_access_lines_method.txt', lines=['access'], methods=['DELETE'], forever=False, count=3)
+	gen.run()
+
+	try:
+		f = open('./test_access_lines_method.txt')
+		lines = f.readlines()
+		for line in lines:
+			# Extract the message field
+			log_msg = re.findall(r'\"(.*?)\"', line)[0]
+			# Extract the http method
+			log_method = log_msg.split()[0]
+			assert log_method == 'DELETE'
+	except:
+		assert False
+
+
+
+
 
 
 
