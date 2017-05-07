16256f1a9072168b7ca4c7acfa8718320033a3bc
xuwenyihust <1016635786@qq.com>
2017-05-06 21:44:55 -0700

    Added tests.

--- src/log_generator.py
+++ src/log_generator.py
@@ -12,13 +12,24 @@ import numpy
 # Apache Access Logs
 class apache(object):
 
-	def __init__(self, out_path='./log/apache.log'):
+	def __init__(self, out_path='./apache.log', lines=['heartbeat', 'access'], methods=['GET', 'POST', 'PUT', 'DELETE'], forever=True, count=1):
+		# Assign the lines to generate
+		self.lines = lines
+		self.lines_full = ['heartbeat', 'access']
+		self.lines_gen = [self.heartbeat_lines(), self.access_lines()]
+		# Assign the http methods to generate
+		self.methods = methods	
+		# Run forever or not
+		self.forever = forever
+		# Total # of logs to generate
+		self.count = count
+
 		# Instantiate the logger
 		self.log = logging.getLogger('Gen')
 		# Set the level
 		logging.basicConfig(level=logging.INFO)
 		# Instantiate a file Handler
-		out = logging.FileHandler(out_path)
+		out = logging.FileHandler(out_path, mode='w')
 
 		log_format = logging.Formatter("%(message)s")
 		# Set the Formatter for this Handler to form
@@ -31,27 +42,26 @@ class apache(object):
 		self.loop = asyncio.get_event_loop()
 		try:
 			self.loop.run_until_complete(
-				asyncio.wait([
-					#self.heartbeat_lines()]
-					self.heartbeat_lines(),
-					self.access_lines()]
-				)
+				asyncio.wait([self.lines_gen[self.lines_full.index(x)] for x in self.lines])
 			)
 		finally:
 			self.loop.close()
 
 
+
 	@coroutine
 	def heartbeat_lines(self):
-		while True:
+		while self.forever or self.count > 0:
 			t = datetime.datetime.now().strftime('%d/%b/%Y:%H:%M:%S -0700')	
 			self.log.info('- - - [%s] "%s" - -', t, 'HEARTBEAT')
+			if self.count > 0:
+				self.count -= 1
 			yield from asyncio.sleep(1)
 
 
 	@coroutine
 	def access_lines(self):
-		while True:
+		while self.forever or self.count > 0:
 			ip = '.'.join(str(random.randint(0, 255)) for i in range(4))
 			user_identifier = '-'
 			user_id = 'frank'
@@ -68,6 +78,8 @@ class apache(object):
 			code = '200'
 			size = random.randint(1024, 10240)
 			self.log.info('%s %s %s [%s] "%s" %s %s', ip, user_identifier, user_id, t, msg, code, size)
+			if self.count > 0:
+				self.count -= 1
 			yield from asyncio.sleep(random.randint(1, 5))
 
 
