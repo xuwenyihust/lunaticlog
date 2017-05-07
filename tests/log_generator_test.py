7091ad3b37d87a801fba26a2fe991feada1f5a95
xuwenyihust <1016635786@qq.com>
2017-05-07 12:50:53 -0700

    Completed test_heartbeat_lines_format().

--- tests/log_generator_test.py
+++ tests/log_generator_test.py
@@ -6,23 +6,27 @@ import os
 import re
 
 
-# Testing Class
-#class test_apache(object):
-
-def test_heartbeat_lines():
+def test_heartbeat_lines_format():
 	gen = apache(out_path='./test_heartbeat_lines.txt', lines=['heartbeat'], forever=False, count=1)
 	gen.run()
 	
 	try:
 		f = open('./test_heartbeat_lines.txt')
 		line = f.readlines()[0]
-		fields = line.split()
-		assert len(fields) == 8
+		#fields = line.split()
+		#assert len(fields) == 8
+		# Extract the time field
+		log_time = re.findall(r'\[(.*?)\]', line)
+		assert len(log_time) == 1
+		# Extract the message field
+		log_msg = re.findall(r'\"(.*?)\"', line)
+		assert len(log_msg) == 1
+		assert log_msg[0] == 'HEARTBEAT'
 	except:
 		assert False	
 
 	
-def	test_access_lines():
+def	test_access_lines_format():
 	gen = apache(out_path='./test_access_lines.txt', lines=['access'], forever=False, count=1)
 	gen.run()
 
