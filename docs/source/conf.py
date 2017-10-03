c66a9fb98dc0d341defaa6f2f4039bf9608121a0
xuwenyihust <1016635786@qq.com>
2017-10-02 19:04:43 -0700

    Theme changes.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,9 +27,9 @@ import sphinx_rtd_theme
 # If your documentation needs a minimal Sphinx version, state it here.
 #
 # needs_sphinx = '1.0'
-#html_theme = "sphinx_rtd_theme"
-#html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-html_theme = "classic"
+html_theme = "sphinx_rtd_theme"
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+#html_theme = "classic"
 #html_theme_options = {
 #    "rightsidebar": "true",
 #    "relbarbgcolor": "black"
