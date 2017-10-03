e45efe34da552d4e76a6680be9ab8353fe39d080
xuwenyihust <1016635786@qq.com>
2017-10-02 21:10:00 -0700

    Theme changes.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -87,8 +87,8 @@ todo_include_todos = False
 # a list of builtin themes.
 #
 #html_theme = 'alabaster'
-#html_theme = "sphinx_rtd_theme"
-html_theme = 'classic'
+html_theme = "sphinx_rtd_theme"
+#html_theme = 'classic'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -100,7 +100,7 @@ html_theme = 'classic'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 #html_static_path = ['_static']
-#html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # -- Options for HTMLHelp output ------------------------------------------
 
